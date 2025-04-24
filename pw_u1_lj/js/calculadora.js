8b05a89eba909b58b0d77140b4de4021be0522bc
Joel <joshua-lunag@hotmail.com>
2025-04-24 16:03:28 -0500

    Taller06

---
+++ pw_u1_lj/js/calculadora.js
@@ -0,0 +1,13 @@
+// es un lenguaje de programacion
+ // comportamiento y o funcionalidad de la pagina web
+ //no maneja una version directa, como CSS3 y HTML5. se basa en un standar ECMAscript (ult ver. ECMAscript2024)
+ // 
+ // manipulacion dimanica de elementos HTML, a traves del DOM (Documento Object Model) 
+ // [estructura en forma de arbol que contiene de manera jerarquica todos los elementos de la pagina web]
+ // [accede modifica agrega o elimina cualquier elemento del DOM]
+ // [siempre hay un elemento raiz "Document"]
+ 
+ // tiene tipos de declaracion: funcionalidad y comportamiento... 
+ //  en linea (a un solo elemento),
+ //  interno (a una sola pagina),
+ //  externo (a varias paginas de manera centralizada).
